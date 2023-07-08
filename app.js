4651b9066d52fde8f07a8912fd494e5924e0813f
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-09 01:27:02 +0300

    feat: new feature

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hello" });
 });
+app.get("/hello", (req, res) => {
+  res.status(200).json({ message: "hello user" })
+});
 
 app.listen(3000, () => {
   console.log("server now is running on port 3000");
