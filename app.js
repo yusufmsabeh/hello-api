cfb4086bb93cd997ccc4227ce58620482889ad1d
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-09 01:28:57 +0300

    fix: add missing semicolon:

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "hello" });
 });
 app.get("/hello", (req, res) => {
-  res.status(200).json({ message: "hello user" })
+  res.status(200).json({ message: "hello user" });
 });
 
 app.listen(3000, () => {
